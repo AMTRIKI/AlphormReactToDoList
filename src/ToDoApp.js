392966d16d4c980fdc05db0b3292b4fef97a33dc
Amel TRIKI <amel.triki.iset@gmail.com>
2021-07-23 20:18:53 +0200

    Partie Front End Finalisée de ToDo App

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -6,10 +6,10 @@ export default class ToDoApp extends Component{
         super(props);
         this.state = {
             items : [ 
-                { text:"item#1", done: false, key: new Date().getMilliseconds()+ "item#1"},
-                { text:"item#2", done: false, key: new Date().getMilliseconds()+ "item#2"},
-                { text:"item#3", done: false, key: new Date().getMilliseconds()+ "item#3"},
-                { text:"item#4", done: false, key: new Date().getMilliseconds()+ "item#4"}
+                { text:"item#1", done: false, key: new Date().getMilliseconds()+ "item #1"},
+                { text:"item#2", done: false, key: new Date().getMilliseconds()+ "item #2"},
+                { text:"item#3", done: false, key: new Date().getMilliseconds()+ "item #3"},
+                { text:"item#4", done: false, key: new Date().getMilliseconds()+ "item #4"}
             ],
             input:''
         }
@@ -40,25 +40,35 @@ export default class ToDoApp extends Component{
         return 
         return undone.length;
     }
+    delet = (key) =>{
+        let filtered = this.state.items.filter (item =>{return item.key!==key})
+        this.setState({items:filtered})
+    }
     render(){
         return(
+            <div>
+                <nav className="navbar navbar-light bg-light">
+                    <div className="container-fluid">
+                        <span className="navbar-brand mb-0 h1">ToDo App</span>
+                    </div>
+                </nav>
         <div className="container">
             <br/>
             <div className="row">
                 <div className="col-md-6">
                     <div className="todolist">
-                    List Undone
                    <form onSubmit={(e)=>{e.preventDefault();this.add()}}>
                        <input placeholder="add to do " 
                        className= "form-control form-control-lg add-todo"
                        value={this.state.input} onChange={(e)=>this.handleChange(e)}/>
                    </form>
-                   
-                    <ul>
+                   <br/>
+                    <ul className="no-padding" id="not-done">
                         {
                             this.state.items.map(item =>{
                                 if(!item.done)
-                                return(<li key={item.key} onClick={()=>this.move(item.key)}>{item.text}</li>)})
+                                return(<li className="list-unstyled" key={item.key}>
+                                    <label onClick={()=>this.move(item.key)}>{item.text}</label></li>)})
                         }
                     </ul>
                     <div className="todo-footer"> 
@@ -68,19 +78,23 @@ export default class ToDoApp extends Component{
      
                 </div>
                 <div className="col-md-6">
-                <div className="todolist">
-                    List Done
-                    <ul>
+
+                <div className="todolist">                    
+                    <ul id="done-items">
                         {
                             this.state.items.map(item =>{
                                 if (item.done)
-                                return(<li key={item.key} onClick={()=>this.move(item.key)}>{item.text}</li>)
+                                return(<li className="list-unstyled"  key={item.key} >
+                                    <label onClick={()=>this.move(item.key)}>{item.text}</label>
+                                    <button className="btn float-right"
+                                    onClick={e=>this.delet(item.key)}><i className="fas fa-trash    "></i></button></li> )
                             })
                         }
                     </ul>
                 </div>
                 </div>
             </div>
+        </div>
         </div>);
     }
 
